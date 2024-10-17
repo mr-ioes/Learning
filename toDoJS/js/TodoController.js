71a42b0ca73b420d6ab77c930b28ee30a5fdbc6d
mr-ioes <107437881+mr-ioes@users.noreply.github.com>
2024-10-17 09:15:31 +0200

    added toDoJS

---
+++ toDoJS/js/TodoController.js
@@ -0,0 +1,28 @@
+export default class TodoController {
+    _model = null;
+    _view = null;
+
+    constructor(model, view) {
+        this._model = model;
+        this._view = view;
+
+        // connect listeners
+
+        this.renderTodos();
+    }
+
+    onItemClick() {
+        //
+    }
+
+    onNewItemSubmit() {
+        //
+    }
+
+    renderTodos() {
+        const todos = this._model.getTodos();
+        this._view.render(todos);
+
+        window.alert("render!") // delete it
+    }
+}
